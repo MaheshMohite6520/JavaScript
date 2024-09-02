6361e05117ebaea51eeafdc82771eb7a62f03e1b
Mahesh Mohite <maheshmohite6520@gmail.com>
2024-09-02 19:33:10 +0530

    new programs done

---
+++ 02-Sep-24/index.js
@@ -0,0 +1,33 @@
+// Scope : var
+
+var a = 10
+if(true)
+{
+  console.log(a);
+  var a = 20
+  console.log(a);
+}
+console.log(a);
+
+
+// Scope : let
+
+// let a = 10
+// if(true)
+// {
+//   console.log(a);
+//   let a = 20
+//   console.log(a);
+// }
+// console.log(a);
+
+// Scope : const
+
+// const a = 10
+// if(true)
+// {
+//   console.log(a);
+//   const a = 20
+//   console.log(a);
+// }
+// console.log(a);
