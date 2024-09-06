19666649e1c4fb5cae99fa211915d2a204300ed5
Mahesh Mohite <maheshmohite6520@gmail.com>
2024-09-06 10:55:36 +0530

    new files added

--- 05-Sep-24/index.js
+++ 05-Sep-24/index.js
@@ -10,17 +10,26 @@ let add = (a, b) => {
   console.log("add called");
   console.log(a + b);
 };
+
 let sub = (a, b) => {
   console.log("sub called");
   console.log(a - b);
 };
+
 let mul = (a, b) => {
   console.log("mul called");
   console.log(a * b);
 };
+
 let div = (a, b) => {
   console.log("div called");
   console.log(a / b);
 };
 
-operation(add, 0, 1);
+operation(add);
+
+operation(sub, 20, 10);
+
+operation(mul);
+
+operation(div, 200, 10);
---
+++ 05-Sep-24/practise.js
@@ -0,0 +1,101 @@
+// Question 1 ---------------->
+
+// console.log("start");
+
+// let a = 10;
+// let b = 20;
+
+// const c = 30;
+
+// {
+//   let a = 100;
+//   let b = 200;
+//   const c = 300;
+
+//   console.log(a); // 100
+//   console.log(b); // 200
+//   console.log(c); // 300
+// }
+
+// console.log(a); // 10
+// console.log(b); // 20
+// console.log(c); // 30
+
+// console.log("end");
+
+// Question 2 ----------------->
+
+// console.log("start");
+
+// let a = 10;
+// console.log(b); // undefined
+// {
+//   var b = 200;
+// }
+// console.log(a); // 10
+// console.log(b); // 200
+// console.log("end");
+
+// Question 3 ----------------->
+
+// console.log("start");
+
+// let a = 10;
+
+// {
+//   // console.log(a); // error
+//   let a = 10;
+// }
+
+// console.log(a); // 10
+// console.log(b); // error
+
+// console.log("end");
+
+// Question 4 ----------------->
+
+// console.log("start");
+
+// var b = 20;
+// const c = 30;
+
+// {
+//   let a = 100;
+
+//   console.log(a); // 100
+//   console.log(b); // 20
+//   console.log(c); // 30
+// }
+
+// // console.log(a); // error
+// console.log(b); // 20
+
+// console.log("end");
+
+// Question 5 ----------------->
+
+console.log("start");
+
+let a = 10;
+var b = 20;
+const c = 30;
+
+{
+  let a = 10;
+  console.log(a); // 10
+
+  const c = 300;
+  console.log(b); // 20
+
+  b = 200;
+  // c = 30; // error
+
+  console.log(b); // 200
+}
+
+console.log(a); // 10
+console.log(b); // 200
+
+console.log("end");
+
+console.log("--------------------------------------------------");
