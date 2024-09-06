ae233f1c6222494397a412b57bfff370e3d90f53
Mahesh Mohite <maheshmohite6520@gmail.com>
2024-09-06 22:56:03 +0530

    new files added

---
+++ 06-Sep-24/index.js
@@ -0,0 +1,21 @@
+// Ways of printing statements in JS
+
+console.log("hello");
+console.error("hello");
+console.warn("hello");
+
+window.console.log("hello again");
+window.console.error("hello again");
+window.console.warn("hello again");
+
+console.log(window);
+
+window.document.write("hello from JS file");
+// window.alert("Kaise ho aap log..........");
+// window.confirm("Click OK");
+
+console.log(document);
+
+// while (true) {
+//   window.prompt("Enter your name");
+// }
