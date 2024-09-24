de630be92c7dcbfbb58a57ae298c7ead946a7fa0
Mahesh Mohite <maheshmohite6520@gmail.com>
2024-09-24 17:40:50 +0530

    new files  added

---
+++ 24-Sep-24/script.js
@@ -0,0 +1,15 @@
+console.log("Object");
+
+let obj = {
+  name: "Tinku",
+  age: 23,
+  contact: 872349832,
+};
+
+let keys = Object.keys(obj);
+console.log(keys);
+
+let values = Object.values(obj);
+console.log(values);
+
+// Presentation in class
