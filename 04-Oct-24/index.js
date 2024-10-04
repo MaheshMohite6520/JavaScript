21ec99f21d2469ae42b5f803a4b645b0c996cbbd
Mahesh Mohite <maheshmohite6520@gmail.com>
2024-10-04 23:03:15 +0530

    hiii

--- 04-Oct-24/index.js
+++ 04-Oct-24/index.js
@@ -1,11 +1,20 @@
-//! Strings ------------>
+//! <------------------------------ Strings ------------------------->
 
-//! Declaring a String --------->
+//! There are 4 ways to Declare an String --------->
 
-//! 1] let str = "Bahubali"
-//! 2] let str = 'Bahubali'
-//! 3] let str = `Bahubali`
-//! 4] let str = new String("Bahubali")
+//! Way - 1
+// let str = "Bahubali"
+
+//! Way - 2
+// let str = 'Bahubali'
+
+//! Way - 3
+// let str = `Bahubali`
+
+//! Way - 4
+// let str = new String("Bahubali")
+
+// console.log("------------------------");
 
 // let str = "Bahubali";
 
@@ -20,12 +29,12 @@
 // console.log(str[8]);
 // console.log(str[9]);
 
-//! Q. Create bablu from Bahubali ------------->
+//! Create bablu from Bahubali ------------->
 
 // let newStr = str[0] + str[1] + str[4] + str[5] + str[6] + str[3];
 // console.log(newStr);
 
-//! Print the string by using for loop --------->
+//! Print the String by using for loop --------->
 
 // let str = "Bahubali";
 // console.log(str);
@@ -37,43 +46,34 @@
 
 // let str = "Bahubali";
 
-// str.forEach((element) => {
+// str.forEach((element) => { //! index.js:33 Uncaught TypeError: str.forEach is not a function
 //   console.log(element);
 // });
 
-//! index.js:33 Uncaught TypeError: str.forEach is not a function
-
-// str.map((element) => {
+// str.map((element) => { //! index.js:45 Uncaught ReferenceError: Cannot access 'str' before initialization
 //   console.log(element);
 // });
 
-//! index.js:45 Uncaught ReferenceError: Cannot access 'str' before initialization
-
-// str.filter((element) => {
+// str.filter((element) => {  //! index.js:52 Uncaught TypeError: str.filter is not a function
 //   console.log(element);
 // });
 
-//! index.js:52 Uncaught TypeError: str.filter is not a function
-
-// str.reduce((element) => {
+// str.reduce((element) => { //! index.js:58 Uncaught TypeError: str.reduce is not a function
 //   console.log(element);
 // });
 
-//! index.js:58 Uncaught TypeError: str.reduce is not a function
-
-// str.sort((element) => {
+// str.sort((element) => {  //! Uncaught TypeError: str.sort is not a function
 //   console.log(element);
 // });
-
-//! Uncaught TypeError: str.sort is not a function
 
 //! Convert the String into an array ------------->
 
-//! 1] split() :
+//! 1] split(character) :
+
 //! It is an string method.
 //! It is used to convert the String into an array.
-//! It will return an array
-//! We have to pass an argument to split() method to split by specific character
+//! It will return an array.
+//! We have to pass an argument to split() method to split by specific character.
 
 // let str = "Bahubali"
 
@@ -83,11 +83,13 @@
 // let strArr = str.split("i");
 // console.log(strArr);
 
-// let str = "Bahubali";
-// console.log(str);
-
 //! 2] reverse() :
-//! It is an array method used to reverse the array
+
+//! It is an array method used to reverse the array.
+//! It will return an array.
+//! It will modify the original arrray
+
+// let str = "Bahubali";
 
 // let arr = str.split("").reverse();
 // console.log(arr);
@@ -95,31 +97,47 @@
 // let newStr = arr.join("");
 // console.log(newStr);
 
-//! 3] join() :
-//! It is an array method used to convert the array into String.
-//! We have to pass an argument to join() method that will convert the array into an String
+// let arr = [10, 20, 30, 40, 50];
+// let newArr = arr.reverse();
+// console.log(newArr);
+// console.log(arr);
+
+//! 3] join(character) :
+
+//! It is an array method used to convert the array into an String.
+//! We have to pass an argument to join() method that will convert the array into an String.
 
 // let str2 = str.split("").reverse().join("");
 // console.log(str2);
 
-//! 4] padStrt()
-//! 5] padEnd()
+//! 4] padStart(length, character that you want to add) :
+
+//! It is used to add the characters at the start of the String.
+//! It will return an new String.
+
+//! 5] padEnd(ength, character that you want to add) :
+
+//! It is used to add the characters at the end of the String.
+//! It will return an new String.
 
 // let num = "6520";
 // let newNo = num.padStart(12, "*").padEnd(20, "*");
 // console.log(newNo);
 
-//! 6] trim()
-//! It will remove the white spaces from start and end(both)
-//! It will return an trimmed string
+//! 6] trim() :
 
-//! 7] trimStart()
-//! It will remove the white spaces from start only
-//! It will return an trimmed string
+//! It will remove the white spaces from start and end(both).
+//! It will return an trimmed string.
 
-//! 8] trimEnd()
-//! It will remove the white spaces from end only
-//! It will return an trimmed string
+//! 7] trimStart() :
+
+//! It will remove the white spaces from start only.
+//! It will return an trimmed String.
+
+//! 8] trimEnd() :
+
+//! It will remove the white spaces from end only.
+//! It will return an trimmed String.
 
 // let str = "      Bahubali       ";
 // console.log(str);
@@ -133,21 +151,25 @@
 // let both = str.trim();
 // console.log(both);
 
-//! 9] toUpperCase()
-//! It is used to the String into upper case
-//! It will return an String
+//! 9] toUpperCase() :
 
-//! 10] toLowerCase()
-//! It is used to the String into upper case
-//! It will return an String
+//! It is used to the String into upper case.
+//! It will return an String.
 
-//! 11] toLocaleLowerCase()
-//! It is used to the String into upper case
-//! It will return an String
+//! 10] toLowerCase() :
 
-//! 12] toLocaleUpperCase()
-//! It is used to the String into lower case
-//! It will return an String
+//! It is used to the String into upper case.
+//! It will return an String.
+
+//! 11] toLocaleLowerCase() :
+
+//! It is used to the String into upper case.
+//! It will return an String.
+
+//! 12] toLocaleUpperCase() :
+
+//! It is used to the String into lower case.
+//! It will return an String.
 
 // let str = "Bahubali";
 // console.log(str);
@@ -161,22 +183,24 @@
 // console.log(str.toLocaleLowerCase());
 // console.log(str.toLocaleUpperCase());
 
-//! 13] replace(removedCharacter, addedCharacter)
-//! It is uesd remove the character from String and add the character in place of that
-//! It will return an new String
-//! It will only replace the first occurence of the character in the String
+//! 13] replace(removedCharacter, addedCharacter) :
 
-//! 14] replaceAll(removedCharacter, addedCharacter)
-//! It is uesd remove the character from String and add the character in place of that
-//! It will return an new String
-//! It will replace the all occurence of the character in the String
+//! It is uesd remove the character from String and add the character in place of that character.
+//! It will return an new String.
+//! It will only replace the first occurence of the character in the String.
 
-let str = "Bahubali";
-console.log(str);
+//! 14] replaceAll(removedCharacter, addedCharacter) :
 
-console.log(str.replace("a", "i"));
-console.log(str.replaceAll("a", "i"));
+//! It is uesd remove the character from String and add the character in place of that character.
+//! It will return an new String.
+//! It will replace the all occurence of the character in the String.
 
-console.log(
-  str.replace("Ba", "K").replace("u", "au").replace("b", "g").replace("l", "ll")
-);
+// let str = "Bahubali";
+// console.log(str);
+
+// console.log(str.replace("a", "i"));
+// console.log(str.replaceAll("a", "i"));
+
+// console.log(
+//   str.replace("Ba", "K").replace("u", "au").replace("b", "g").replace("l", "ll")
+// );
---
+++ 04-Oct-24/practise.js
@@ -0,0 +1,45 @@
+//! 1. Write a program to find square and cube of a given number.
+
+// function squareAndCube(num) {
+//   let square = num * num;
+//   let cube = num * num * num;
+
+//   return { square, cube };
+// }
+
+// let value = squareAndCube(10);
+// console.log(value);
+
+//! 2. Write a program to check if a given year is a leap year or not.
+
+// let leapYear = function (year) {
+//   if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+//     return "Leap Year";
+//   } else {
+//     return "Not Leap Year";
+//   }
+// };
+
+// console.log(leapYear(1900));
+
+//! 3. Write a program to calculate the sum of the first 100 natural numbers.
+
+// let num = 100;
+// let sum = (num * (num + 1)) / 2;
+// console.log(sum);
+
+//! 4. Write a program to check if a given number is even or odd.
+
+// let num = 10;
+
+// function evenOdd(num) {
+//   if (num % 2 == 0) {
+//     return "Even";
+//   } else {
+//     return "Odd";
+//   }
+// }
+
+// console.log(evenOdd(num));
+
+//! 5. Write a program to print the sum of all even numbers from 1 to any given number.
