81e682b319c349a8e4fa95e7ff707a5da0dd4214
Mahesh Mohite <maheshmohite6520@gmail.com>
2024-10-05 13:52:32 +0530

    new files added

---
+++ 05-Oct-24/exception.js
@@ -0,0 +1,10 @@
+//! <-------------------------- Exception Handling -------------------------->
+
+try {
+  // console.log(a);
+  throw new Error("a is not defined");
+} catch (err) {
+  console.log(err);
+} finally {
+  console.log("Hello World...");
+}
---
+++ 05-Oct-24/index.js
@@ -0,0 +1,93 @@
+//! <------------------------ String Methods ---------------------->
+
+// let str = "Katappa";
+// console.log(str);
+
+//! 15] slice(start index, end index) :
+
+//! It is used to copy the string from original string.
+//! It will return an new String.
+//! It will not modify the original String.
+
+// let val = str.slice(0, 2);
+// console.log(val);
+
+// let val1 = str.slice(-1);
+// console.log(val1);
+
+// let val2 = str.slice(-1, str.length);
+// console.log(val2);
+
+//! 16] subString(start index, end index) :
+
+// let str = "Katappa";
+// console.log(str);
+
+// let val = str.substring(-1);
+// console.log(val);
+
+// let val1 = str.substring(0);
+// console.log(val1);
+
+//! 17] substr(start index, length of characters that you want to delete);
+
+// let str = "Katappa";
+// console.log(str);
+
+// let val = str.substr(-7, 4);
+// console.log(val);
+
+//! 18] includes("character") :
+
+//! It is used to find a character in a String.
+//! It will return an boolean value.
+
+// let str = "Katappa";
+// console.log(str);
+
+// let val = str.includes("K");
+// console.log(val);
+
+// let val1 = str.includes("appa");
+// console.log(val1);
+
+//! 19] startsWith(character) :
+//! 20] endsWith(character) :
+
+//! Both methods are used to check wheather the String is starting or ending with particular string or not.
+//! It will return an boolean value.
+
+// let str = "Katappa";
+// console.log(str);
+
+// let val1 = str.startsWith("k");
+// console.log(val1);
+
+// let val2 = str.endsWith("ppa");
+// console.log(val2);
+
+//! 21] at(index) :
+//! 22] charAt(index) :
+
+//! Both methods are used to find the character present at particular index.
+//! It will return an character.
+
+// let str = "Katappa";
+// console.log(str);
+
+// let val = str.at(4);
+// console.log(val);
+
+// let val = str.charAt(4);
+// console.log(val);
+
+//! 23] charCodeAt(index) :
+
+//! It is used to find the ASCII value of an character present in the String.
+//! It will return an ASCII value.
+
+// let str = "Katappa";
+// console.log(str);
+
+// let ascii = str.charCodeAt(4);
+// console.log(ascii);
